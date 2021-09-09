367a2643e478a9f6b458f7153bf614114c80f928
Gabriel R <gts.senna@gmail.com>
2021-09-09 16:13:41 -0300

    primeira implmentação da tela

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,62 @@
 <template>
   <div id="app">
     <div id="urna">
-      <Teclado/>
+      <Tela
+        :tela="tela"
+        :numeroVoto="numeroVoto"
+        :quantidadeNumeros="quantidadeNumeros"
+      />
+      <Teclado />
     </div>
   </div>
 </template>
 
 <script>
-import '@/css/global.css';
-import Teclado from '@/components/Teclado.vue';
+import "@/css/global.css";
+import Teclado from "@/components/Teclado.vue";
+import Tela from "@/components/Tela.vue";
+import candidatos from "@/assets/candidatos.js"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Teclado
-  }
-}
+    Teclado,
+    Tela,
+  },
+  methods: {
+    adicionarNumer(numero){
+      //Adiciona o número selecionado
+      this.numeroVoto += ''+numero;
+    }
+  },
+  data() {
+    return {
+      tela: "prefeito",
+      numeroVoto: "",
+      quantidadeNumeros: 2,
+      candidato: {},
+      candidatos
+    };
+  },
+};
 </script>
 
 <style>
-  #app {
-    background-color: var(--background-color);
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  #urna{
-    width: 1000px;
-    height: 500px;
-    background-color: var(--ballot-box-background-color);
-    padding: 30px;
-    border-radius: 5px;
-  }
+#app {
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#urna {
+  display: flex;
+  justify-content: space-between;
+  width: 1000px;
+  height: 500px;
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+  border-radius: 5px;
+}
 </style>
---
+++ src/components/Tela.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="urna-tela">
+    <div v-if="tela != 'fim'" class="urn-tela-voto">
+      <div class="urna-tela-voto-textos">
+        <div class="urna-tela-voto-titulo">Seu voto para:</div>
+        <div class="urna-tela-voto-tipo">{{ tela }}</div>
+      </div>
+      <div class="urna-tela-voto-numeros">
+        Números
+        <div
+          class="urna-tela-voto-numero"
+          v-for="(value, key) in numeroVoto.padEnd(quantidadeNumeros, '0')"
+          :key="key"
+        >
+          {{ value }}
+        </div>
+      </div>
+      votação
+    </div>
+
+    <div class="urna-tela-voto-imagem"></div>
+
+    <div class="urna-tela-voto-instrucoes"></div>
+
+    <div v-if="tela == 'fim'" class="urna-tela-fim">
+      finalização
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tela",
+  props: {
+    tela: String,
+    numeroVoto: String,
+    quantidadeNumeros: Number,
+  },
+};
+</script>
+
+<style>
+.urna-tela {
+  width: 55%;
+  height: 100%;
+  background-color: var(--ballot-box-screen-color);
+  border-radius: 5px;
+  border: 2px solid var(--light-border-color);
+  padding: 20px;
+  color: var(--dark-text-color);
+}
+
+.urna-tela-voto-titulo {
+  font-size: 20px;
+}
+
+.urna-tela-voto-tipo {
+  font-size: 30px;
+  font-weight: bold;
+  margin: 20px 0;
+  text-transform: uppercase;
+}
+
+.urna-tela-voto-numeros{
+    display: flex;
+    align-items: center;
+}
+
+.urna-tela-voto-numero {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 45px;
+    height: 45px;
+    border: 1px solid var(--dark-border-color);
+    margin-left: 10px;  
+    font-size: 30px;
+}
+</style>
